e76cde7073f3047403cce6ef944496f5ea2c2481
unknown <xxx_darkforces@mail.ru>
2022-11-30 15:23:33 +0700

    edit

--- init.js
+++ init.js
@@ -3,5 +3,7 @@ window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surnameOutput').innerText = initPerson.surnameName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
 };
 
--- personGenerator.js
+++ personGenerator.js
@@ -35,6 +35,21 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Ирина",
+            "id_2": "Светлана",
+            "id_3": "Алиса",
+            "id_4": "Екатерина",
+            "id_5": "Виктория",
+            "id_6": "Наталья",
+            "id_7": "Елена",
+            "id_8": "Надежда",
+            "id_9": "Ефросинья",
+            "id_10": "Татьяна"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -47,24 +62,28 @@ const personGenerator = {
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+    randomFirstName: function () {
+        if (this.person.gender == this.GENDER_MALE) {
+            return this.randomValue(this.firstNameMaleJson);
+        } else if (this.person.gender == this.GENDER_FEMALE) {
+            return this.randomValue(this.firstNameFemaleJson);
+        }
     },
 
-
-     randomSurname: function() {
-
-        return this.randomValue(this.surnameJson);
-
+    randomSurname: function () {
+        if (this.person.gender == this.GENDER_MALE) {
+            return this.randomValue(this.surnameJson);
+        } else if (this.person.gender == this.GENDER_FEMALE) {
+            return this.randomValue(this.surnameJson) + 'a';
+        }
     },
 
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomIntNumber() === 1 ? this.GENDER_MALE : this.GENDER_FEMALE;
         this.person.firstName = this.randomFirstName();
+        this.person.surnameName = this.randomSurname();
         return this.person;
     }
 };
